35d961d08d1729a6e0f75e54680b79dff7989c0a
rjAtlasDigitalAi <rjatlasdigitalaitly2@gmail.com>
2026-05-22 16:52:32 +0530

    new commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const formRoutes = require('./routes/form');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'https://courseform-chi.vercel.app' }));
 app.use(express.json());
 app.use('/api', formRoutes);
 
